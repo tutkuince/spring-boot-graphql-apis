57362a503c5c75df7e5a2137b0eb22ca8930b027
tutkuince <tutku.ince@outlook.com>
2019-12-06 12:27:50 +0300

    initial commit

---
+++ src/main/java/com/muditasoft/springbootgraphqlapis/SpringBootGraphqlApisApplication.java
@@ -0,0 +1,13 @@
+package com.muditasoft.springbootgraphqlapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGraphqlApisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muditasoft/springbootgraphqlapis/SpringBootGraphqlApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muditasoft.springbootgraphqlapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
